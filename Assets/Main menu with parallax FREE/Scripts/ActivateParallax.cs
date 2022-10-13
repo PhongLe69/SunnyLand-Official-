0f5814d2361ccc1f57b7c463640218f354f14246
Phong Le <69zemo@gmail.com>
2022-10-14 01:23:10 +0700

    Create Menu
    Use Canvas and UI to make the simple Menu with 3 choices
     - Play
     - Options
      + Volume
      + Back
     - Quit

---
+++ Assets/Main menu with parallax FREE/Scripts/ActivateParallax.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateParallax : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+	 if(gameObject.transform.GetSiblingIndex() == 0)
+        {
+          var parallaxx = gameObject.transform.GetComponentsInChildren<Parallaxx>();
+            for (int i = 0; i < parallaxx.Length; i++)
+            {
+                parallaxx[i].isActive = true;
+            }
+        }else
+        {
+            var parallaxx = gameObject.transform.GetComponentsInChildren<Parallaxx>();
+            for (int i = 0; i < parallaxx.Length; i++)
+            {
+                parallaxx[i].isActive = false;
+            }
+        }
+	}
+}
