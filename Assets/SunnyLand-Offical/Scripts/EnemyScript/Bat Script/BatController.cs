340985c1e615ef26deba929329d30d20e3a64a57
Phong Le <69zemo@gmail.com>
2022-11-25 01:00:27 +0700

    BatController_FirstUpdate
    - Movement of bat
    - sprite and animator (idle and fly)
    - Use BezierFollow to make the movement of bat.
    - Use Trigger collider2D to detect the Bat when player collider with bat

---
+++ Assets/SunnyLand-Offical/Scripts/EnemyScript/Bat Script/BatController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : MonoBehaviour
+{
+    /*protected Animator animator;
+    //public BoxCollider2D collider2D;
+    private float spawnDelay = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        StartCoroutine(Twinkling());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            animator.SetBool("isFly", true);
+            animator.SetBool("isIdle", false);
+
+            //collider2D.enabled = false;
+            //animator.SetTrigger("isFly");
+        }
+    }
+    IEnumerator Twinkling()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        animator.SetBool("isFly", false);
+        animator.SetBool("isIdle", true);
+
+        //collider2D.enabled = true;
+        //animator.SetTrigger("isIdle");
+        StartCoroutine(Twinkling());
+    }*/
+
+}
